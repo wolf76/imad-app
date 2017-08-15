9bb3c10afe14f879e0990b0fb36b090cc0355e6a
wolf76 <iamnarey@gmail.com>
2017-08-15 08:27:49 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/atricle-one)', function (req,res) {
+   res.send('Article ONE will be served here...'); 
+});
+
+app.get('/atricle-two)', function (req,res) {
+   res.send('Article TWO will be served here...'); 
+});
+
+app.get('/atricle-three)', function (req,res) {
+   res.send('Article THREE will be served here...'); 
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
