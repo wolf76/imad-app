c3fb40054e303a4e02364dbf5710de3c9aa81e17
wolf76 <iamnarey@gmail.com>
2017-08-15 08:33:04 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,15 +9,15 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
-app.get('/atricle-one', function (req,res) {
+app.get('/article-one', function (req,res) {
    res.send('Article ONE will be served here...'); 
 });
 
-app.get('/atricle-two', function (req,res) {
+app.get('/article-two', function (req,res) {
    res.send('Article TWO will be served here...'); 
 });
 
-app.get('/atricle-three', function (req,res) {
+app.get('/article-three', function (req,res) {
    res.send('Article THREE will be served here...'); 
 });
 
